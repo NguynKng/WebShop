7929878d1df1d6a9b981fcb20e20a007e45c99b1
NguynKng <harrybin2109@gmail.com>
2024-02-29 10:37:23 +0700

    update 29/2

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, json, redirect, url_for
+from flask import Flask, render_template, request, json, redirect, url_for, jsonify, session
 from flask_mysqldb import MySQL
-import dao
+import dao, os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 mysql = MySQL()
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
@@ -15,30 +16,22 @@ mysql.init_app(app)
 def home():
     return render_template("home.html")
 
-@app.route('/showSignUp')
-def showSignUp():
-    return render_template("signup.html")
-
-@app.route("/signup",methods=["POST"])
+@app.route("/signup",methods=["POST","GET"])
 def signup():
     if request.method == "POST":
         _username = request.form['username']
         _password = request.form['password']
         _email = request.form['email']
-
         cursor = mysql.connection.cursor()
         cursor.execute("INSERT INTO demo.customer (user_username,user_password,user_email) VALUES (%s,%s,%s)",(_username,_password,_email))
         mysql.connection.commit()
         cursor.close()
-        return showLogin()
-    return render_template("index.html")
+        return redirect(url_for("login"))
+    return render_template("signup.html")
 
-@app.route("/showLogin")
-def showLogin():
-    return render_template("login.html")
-
-@app.route("/login",methods=["POST"])
+@app.route("/login",methods=["POST","GET"])
 def login():
+    error = ""
     if request.method == "POST":
         _username = request.form['username']
         _password = request.form['password']
@@ -48,32 +41,99 @@ def login():
         for row in data:
             if _username == row[1]:
                 if _password == row[2]:
-                    return "Đăng nhập thành công."
+                    session['user'] = row
+                    return redirect(url_for('home'))
         else:
             cursor.close()
-            return "Sai tài khoản hoặc mật khẩu, vui lòng thử lại!."
-@app.route("/products")
+            error = "Sài tài khoản hoặc mật khẩu! Vui lòng thử lại."
+    return render_template('login.html',error=error)
+
+@app.route("/logout")
+def logout():
+    session['user'] = None
+    return redirect(url_for('home'))
+
+@app.route("/products/")
 def product_list():
     kw = request.args.get("keyword")
     from_price = request.args.get("from_price")
     to_price = request.args.get("to_price")
+    category_id = request.args.get("category_id")
     return render_template("products.html",
-                           products=dao.read_products(keyword=kw,from_price=from_price,to_price=to_price))
-
-@app.route("/products/add",methods=["POST","GET"])
-def add_products():
-    error = ""
+                           products=dao.read_products(category_id=category_id,keyword=kw,from_price=from_price,to_price=to_price),categories = dao.read_categories())
+@app.route("/products/<int:category_id>")
+def category_list(category_id):
+    products = dao.read_product_by_category_id(category_id)
+    return render_template("products.html",
+                           products=products)
+'''
+@app.route("/api/products",methods=["GET","POST"])
+def api_product_list():
     if request.method == "POST":
-        name = request.form.get("name")
-        description = request.form.get("description")
-        price = request.form.get("price")
-        amount = request.form.get("amount")
-        category_id = request.form.get("category_id")
-        if dao.add_product(name=name,description=description,price=price,category_id=category_id,amount=amount):
+        error = ""
+        product_id = request.args.get('product_id')
+        product = None
+        if product_id:
+            product = dao.read_product_id(product_id=int(product_id))
+'''
+
+@app.route('/products/update',methods=["POST","GET"])
+def update_product():
+    error = None
+    product = None
+    if request.method == "POST":
+        name = request.form.get('name')
+        description = request.form.get('description')
+        price = request.form.get('price')
+        category_id = request.form.get('category_id')
+        amount = request.form.get('amount')
+        product_id = request.form.get('product_id')
+        if dao.update_product(
+                name=name,
+                description=description,
+                price=price,
+                amount=amount,
+                product_id=product_id,
+                category_id=category_id):
             return redirect(url_for("product_list"))
         else:
             error = "Something went wrong! PLease back later!."
-    return render_template("add_products.html",categories=dao.read_categories(),error=error)
+
+
+    if request.args.get('product_id'):# Chạy được
+        product = dao.read_product_id(int(request.args['product_id']))
+
+    return render_template('update_products.html', categories=dao.read_categories(), error=error, product=product)
+
+@app.route("/products/add",methods=["POST","GET"])
+def add_product():
+    error = None
+    product = None
+    if request.method == "POST":
+        name = request.form.get('name')
+        description = request.form.get('description')
+        price = request.form.get('price')
+        category_id = request.form.get('category_id')
+        amount = request.form.get('amount')
+        if dao.add_product(name,description,price,category_id,amount):
+            return redirect(url_for("product_list"))
+        else:
+            error = "Something went wrong! PLease back later!."
+
+    return render_template("add_products.html",categories=dao.read_categories(),error=error,product=product)
+
+@app.route('/api/products/<int:product_id>',methods=["DELETE"])
+def delete_product(product_id):
+    if dao.delete_product(product_id=product_id):
+        return jsonify({
+            "status": 200,
+            "message": "Delete successfully",
+            "data": {"product_id": product_id}
+        })
+    return jsonify({
+        "status": 500,
+        "message": "Delete not successfully"
+    })
 
 if __name__ == "__main":
     app.run()
--- dao.py
+++ dao.py
@@ -2,8 +2,8 @@ import json, os, hashlib
 def read_products(category_id=0, keyword=None, from_price=None, to_price=None):
     f = open("data/products.json","r",encoding="utf-8")
     products = json.load(f)
-    if category_id > 0:
-        products = [p for p in products if p["category_id"] == category_id]
+    if category_id:
+        products = [p for p in products if p["category_id"] == int(category_id)]
     if keyword:
         products = [p for p in products if keyword.lower() in p["name"].lower() or keyword.lower() in p["description"].lower()]
     if from_price and to_price:
@@ -15,14 +15,37 @@ def read_categories():
     categories = json.load(f)
     return categories
 
+def read_product_by_category_id(category_id):
+    products = read_products()
+    for p in products:
+        if p["category_id"] == category_id:
+            return p
+    return None
+
+def read_product_id(product_id):
+    products = read_products()
+    for p in products:
+        if p["id"] == product_id:
+            return p
+    return None
 def AI_id():
     lst_id = []
     products = read_products()
     for p in products:
         lst_id.append(p["id"])
-    return max(lst_id)+1
+    return int(max(lst_id))+1
+
+def write_json(products,filename):
+    try:
+        f = open(filename,"w",encoding="utf-8")
+        json.dump(products, f, ensure_ascii=False, indent=4)
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
 
 def add_product(name, description, price, category_id, amount):
+    filename = "data/products.json"
     products = read_products()
     new_product = {
         "id": AI_id(),
@@ -33,13 +56,29 @@ def add_product(name, description, price, category_id, amount):
         "amount": int(amount)
     }
     products.append(new_product)
-    try:
-        f = open("data/products.json","w",encoding="utf-8")
-        json.dump(products, f, ensure_ascii=False, indent=4)
-        return True
-    except Exception as ex:
-        print(ex)
-        return False
+    return write_json(products,filename)
+
+def update_product(name, description, price, amount, product_id, category_id):
+    filename = "data/products.json"
+    products = read_products()
+    for idx, p in enumerate(products):
+        if p["id"] == int(product_id):
+            products[idx]["name"] = name
+            products[idx]["description"] = description
+            products[idx]["price"] = int(price)
+            products[idx]["amount"] = int(amount)
+            products[idx]["category_id"] = int(category_id)
+            break
+    return write_json(products,filename)
+
+def delete_product(product_id):
+    filename = "data/products.json"
+    products = read_products()
+    for idx, p in enumerate(products):
+        if p['id'] == int(product_id):
+            del products[idx]
+            break
+    return write_json(products,filename)
 
 if __name__ == "__main__":
-    print(read_products)
+    print(read_products())
